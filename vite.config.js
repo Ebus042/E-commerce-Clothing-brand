c6046548bfbfa7ba4285d6208740d077d5dc6f5f
Nwafor Ebuka A. <nwaforebuka12@gmail.com>
2025-10-07 09:22:48 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/E-commerce-Clothing-brand/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/E-commerce-Clothing-brand/",
